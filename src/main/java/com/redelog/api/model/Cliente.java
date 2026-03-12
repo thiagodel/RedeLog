ce7f619036988e2d0fcec6c62d0fae782ee7da81
Thiago Delmiro <thiagodelmirorec2@gmail.com>
2026-03-11 22:34:58 -0300

    teste

--- src/main/java/com/redelog/api/model/Cliente.java
+++ src/main/java/com/redelog/api/model/Cliente.java
@@ -1,4 +1,64 @@
 package com.redelog.api.model;
 
 public class Cliente {
+
+    private int id;
+    private String nome;
+    private String telefone;
+    private String endereco;
+    private String cep;
+    private String email;
+
+    public Cliente(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    
+
+
+
+
+
 }
--- src/main/java/com/redelog/api/model/filiais.java
+++ src/main/java/com/redelog/api/model/filiais.java
@@ -1,6 +1,5 @@
 package com.redelog.api.model;
 
-public class Motorista {
-
+public class filiais {
     
 }
