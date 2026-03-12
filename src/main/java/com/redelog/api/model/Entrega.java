059b1225fb1f9b426fd8f3c3178685e659caaf32
Guilherme Augusto Piedade <GuiguiMlk01@gmail.com>
2026-03-11 22:36:01 -0300

    Update Entrega.java

--- src/main/java/com/redelog/api/model/Entrega.java
+++ src/main/java/com/redelog/api/model/Entrega.java
@@ -1,4 +1,14 @@
 package com.redelog.api.model;
 
 public class Entrega {
+    
+    private int id;
+    private string cliente;
+    private string endereço;
+    private string telefone;
+    private string status;
+
+    public Entrega(){}
+
+    
 }
