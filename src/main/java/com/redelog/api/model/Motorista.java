f61c19e0835b5bc9082d55a6716af7a6b723328a
Guilherme Augusto Piedade <GuiguiMlk01@gmail.com>
2026-03-12 22:44:35 -0300

    commit do restante do model

--- src/main/java/com/redelog/api/model/Motorista.java
+++ src/main/java/com/redelog/api/model/Motorista.java
@@ -1,4 +1,12 @@
 package com.redelog.api.model;
 
 public class Motorista {
+
+    private int id;
+    private string cliente;
+    private string endereço;
+    private string telefone;
+    private string statud;
+
+    public Motorista(){}
 }
