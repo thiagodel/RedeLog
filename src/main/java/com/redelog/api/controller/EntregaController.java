4feb8592b01e2feb0a19b52db34786f2866aad07
Thiago Delmiro <thiagodelmirorec2@gmail.com>
2026-04-06 20:27:05 -0300

    Adicionando Controller e Service de Entregas! OBS: metodos ainda não inicializados

--- src/main/java/com/redelog/api/controller/EntregaController.java
+++ src/main/java/com/redelog/api/controller/EntregaController.java
@@ -1,4 +1,37 @@
 package com.redelog.api.controller;
 
+import com.redelog.api.model.entities.Entrega;
+import com.redelog.api.service.EntregaService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 public class EntregaController {
+
+    private final EntregaService entregaService;
+
+    public EntregaController(EntregaService entregaService){
+        this.entregaService = entregaService;
+    }
+
+    @GetMapping
+    public List<Entrega> listar() {
+        return entregaService.listarTodos();
+    }
+
+    @PostMapping
+    public Entrega salvar(@RequestBody Entrega entrega){
+        return entregaService.salvar(entrega);
+    }
+
+    @PutMapping("/{id}")
+    public Entrega atualizar(@PathVariable Integer id, @RequestBody Entrega entrega){
+        return entregaService.atualiza(id, entrega);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Integer id) {
+        entregaService.deletar(id);
+    }
+
 }
--- src/main/java/com/redelog/api/service/EntregaService.java
+++ src/main/java/com/redelog/api/service/EntregaService.java
@@ -1,4 +1,25 @@
 package com.redelog.api.service;
 
+import com.redelog.api.model.entities.Entrega;
+import com.redelog.api.model.entities.Entregador;
+import com.redelog.api.repository.EntregaRepository;
+import com.redelog.api.repository.EntregadorRepository;
+
+import java.util.List;
+
 public class EntregaService {
+
+    private final EntregaRepository entregaRepository;
+    private final EntregadorRepository entregadorRepository;
+
+    public EntregaService(EntregaRepository entregaRepository, EntregadorRepository entregadorRepository){
+        this.entregaRepository = entregaRepository;
+        this.entregadorRepository = entregadorRepository;
+    }
+
+    public List<Entrega> listarTodos(){
+        return entregaRepository.findAll();
+    }
+
+
 }
