d5d473bc2ffe90f71f5b5e600996ded8326751ff
Guilherme Augusto Piedade <GuiguiMlk01@gmail.com>
2026-04-07 22:18:16 -0300

    Update Entregador.java

--- src/main/java/com/redelog/api/model/entities/Entregador.java
+++ src/main/java/com/redelog/api/model/entities/Entregador.java
@@ -2,5 +2,43 @@ package com.redelog.api.model.entities;
 
 public class Entregador {
 
+    private string nome;
+    private string telefone;
+    private string email;
+    private string placaVeiculo;
+    private boolean ativo;
+    public entregador(){}
+
+    public id (String nome, string telefone, string email, string placaVeiculo) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this. email = email;
+        this.placaVeiculo = placaVeiculo;
+        this.ativo = true;
+    } 
+
+    public void desativar(){
+        this.ativo = false;
+    }
+
+    public void ativar(){
+        this.ativo = true;
+    }
+
+    public void atualizarDados(string nome, string telefone, string email, string placaVeiculo){
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+        this.placaVeiculo = placaVeiculo;
     
+    }
+
+    public void atualizarTelefone(String novoTelefone){
+        this.telefone = novoTelefone;
+         system.out.println("Telefone atualizado para: " + novoTelefone);
+        
+    }
 }
+
+    
+
