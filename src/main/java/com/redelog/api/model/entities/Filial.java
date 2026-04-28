95d595f76d1f9890360cf5ef53d2fbd90a9609de
Guilherme Augusto Piedade <GuiguiMlk01@gmail.com>
2026-04-27 22:26:19 -0300

    commitando algumas modificações e adições

--- src/main/java/com/redelog/api/model/entities/Filial.java
+++ src/main/java/com/redelog/api/model/entities/Filial.java
@@ -1,5 +1,23 @@
 package com.redelog.api.model.entities;
 
+ 
+
 public class Filial {
-    
+    private long id;
+    private string nome;
+    private string endereco;
+    private string numeroFilial;
+    public Filial() {
+    }
+
+public Filial(long id, string nome, string endereco, string numeroFilial){
+    this.id = id;
+    this.nome = nome;
+    this.endereco = endereco;
+    this.numeroFilial = numeroFilial;
+
+   
 }
+}
+
+    
