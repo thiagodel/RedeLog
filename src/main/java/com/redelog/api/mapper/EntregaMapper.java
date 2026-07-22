1e16568842c16c5d35841a737424a8e819ab4f7c
Thiago Delmiro <thiagodelmirorec2@gmail.com>
2026-07-21 21:10:24 -0300

    V2 thiago

--- src/main/java/com/redelog/api/mapper/EntregaMapper.java
+++ src/main/java/com/redelog/api/mapper/EntregaMapper.java
@@ -1,6 +1,8 @@
 package com.redelog.api.mapper;
 
+import com.redelog.api.dto.EnderecoResponseDTO;
 import com.redelog.api.dto.EntregaResponseDTO;
+import com.redelog.api.model.entities.Endereco;
 import com.redelog.api.model.entities.Entrega;
 
 public class EntregaMapper {
@@ -13,11 +15,26 @@ public class EntregaMapper {
         dto.setNomeCliente(entrega.getCliente() != null ? entrega.getCliente().getNome() : null);
         dto.setNomeEntregador(entrega.getEntregador() != null ? entrega.getEntregador().getNome() : null);
         dto.setFilialOrigem(entrega.getFilialOrigem() != null ? entrega.getFilialOrigem().getNome() : null);
-        dto.setEnderecoEntrega(entrega.getEnderecoEntrega());
         dto.setStatus(entrega.getStatus() != null ? entrega.getStatus().name() : null);
         dto.setDataCriacao(entrega.getDataCriacao());
         dto.setDataEnvio(entrega.getDataEnvio());
         dto.setDataEntrega(entrega.getDataEntrega());
+
+        if (entrega.getEnderecoEntrega() != null) {
+            Endereco endereco = entrega.getEnderecoEntrega();
+
+            EnderecoResponseDTO enderecoDTO = new EnderecoResponseDTO();
+            enderecoDTO.setRua(endereco.getRua());
+            enderecoDTO.setNumero(endereco.getNumero());
+            enderecoDTO.setBairro(endereco.getBairro());
+            enderecoDTO.setCidade(endereco.getCidade());
+            enderecoDTO.setEstado(endereco.getEstado());
+            enderecoDTO.setCep(endereco.getCep());
+            enderecoDTO.setComplemento(endereco.getComplemento());
+
+            dto.setEnderecoEntrega(enderecoDTO);
+        }
+
         return dto;
     }
 
